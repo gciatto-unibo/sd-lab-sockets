bc463c9e30d938b6b7d6c5bfacca559aede02af1
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-10-17 16:34:53 +0200

    fix: errors in snippets

--- listings/bufferedRead.java
+++ listings/bufferedRead.java
@@ -1,13 +1,12 @@
-static final int BUFFER_SIZE = 1024;
+static final byte[] buffer = new byte[BUFFER_SIZE];
+static final Socket socket = /* obtained somehow  */;
 
-void bufferedRead(Socket socket) {
-    // buffer for holding chunks of read data
-    byte[] buffer = new byte[BUFFER_SIZE];
-    
+void bufferedRead(Socket socket) {    
     // get the socket's input stream (with automatic closure)
-    try(InputStream inputStream = socket.getInputStream()) {
+    try {
+        InputStream inputStream = socket.getInputStream()
         while (true) {
-            // attempts to read at most BUFFER_SIZE bytes,
+            // attempts to read at most buffer.length bytes,
             // readBytes contains the amount of bytes actually read
             int readBytes = inputStream.read(buffer);
             if (readBytes < 0) { // if the stream is over...
@@ -17,14 +16,13 @@ void bufferedRead(Socket socket) {
                 onChunkRead(buffer, readBytes);
             }
         }
-    } catch (IOException e) { 
-        // may occurr because the connection is closed...
+    } catch (IOException e) { // may occurr because the connection is closed...
         onReadingError(e);
-    } /* finally {                 // IMPLICIT
-        inputStream.close()        // IMPLICIT 
-    } */                           // IMPLICIT
+    } finally {
+        socket.shutdownInput()
+    }
 }
-
+(*@\framebreak@*)
 void onChunkRead(byte[] buffer, int size) { /* handle size new bytes */ }
 
 void onReadingOver() { /* handle no more data to read */ }
--- listings/writeData.java
+++ listings/writeData.java
@@ -1,15 +1,13 @@
 void writeData(Socket socket, byte[] data) {   
     try {
-        try {
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(data, 0, data.length);
-            outputStream.flush();
-            // avoid closing the stream: 
-            // this would prevent subsequent writes
-        } catch (IOException e) {
-            onWritingError(e);
-        } 
-    }
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write(data, 0, data.length);
+        outputStream.flush();
+        // avoid closing the stream: 
+        // this would prevent subsequent writes
+    } catch (IOException e) {
+        onWritingError(e);
+    } 
 }
 
 void onWritingError(IOException e) { /* handle e */ }
