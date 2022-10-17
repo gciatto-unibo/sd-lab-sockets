a191a6dac9d5c4075cb61a07a9b40dc482d578ed
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-10-17 15:43:24 +0200

    fix: minor improvements

--- listings/bufferedRead.java
+++ listings/bufferedRead.java
@@ -4,25 +4,25 @@ void bufferedRead(Socket socket) {
     // buffer for holding chunks of read data
     byte[] buffer = new byte[BUFFER_SIZE];
     
-    try {
-        // get the socket's input stream (with automatic closure)
-        try(InputStream inputStream = socket.getInputStream()) {
-            while (true) {
-                // attempts to read at most BUFFER_SIZE bytes,
-                // readBytes contains the amount of bytes actually read
-                int readBytes = inputStream.read(buffer);
-                if (readBytes < 0) { // if the stream is over...
-                    onReadingOver();
-                    return; // interrupts the reading
-                } else { // otherwise, if some bytes have been read
-                    onChunkRead(buffer, readBytes);
-                }
+    // get the socket's input stream (with automatic closure)
+    try(InputStream inputStream = socket.getInputStream()) {
+        while (true) {
+            // attempts to read at most BUFFER_SIZE bytes,
+            // readBytes contains the amount of bytes actually read
+            int readBytes = inputStream.read(buffer);
+            if (readBytes < 0) { // if the stream is over...
+                onReadingOver();
+                return; // interrupts the reading
+            } else { // otherwise, if some bytes have been read
+                onChunkRead(buffer, readBytes);
             }
-        } catch (IOException e) { 
-            // may occurr because the connection is closed...
-            onReadingError(e);
-        } 
-    }
+        }
+    } catch (IOException e) { 
+        // may occurr because the connection is closed...
+        onReadingError(e);
+    } /* finally {                 // IMPLICIT
+        inputStream.close()        // IMPLICIT 
+    } */                           // IMPLICIT
 }
 
 void onChunkRead(byte[] buffer, int size) { /* handle size new bytes */ }
